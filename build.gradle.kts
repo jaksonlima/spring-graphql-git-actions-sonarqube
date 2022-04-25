dcf01eb63e8dad82c7a49f67562e82581756b3da
Your Name <you@example.com>
2022-04-25 00:24:27 -0300

    SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.6"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+    id("org.springframework.boot") version "2.6.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    id("org.sonarqube") version "3.3"
+    id("jacoco")
 }
 
 group = "com.spring"
@@ -12,30 +14,47 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-  maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.experimental:graphql-spring-boot-starter:1.0.0-M2")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.experimental:graphql-spring-boot-starter:1.0.0-M2")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+//sonarqube {
+//    properties {
+//        property("sonar.projectKey", "spring-kotlin-graphql")
+//        property("sonar.host.url", "http://localhost:9000")
+//        property("sonar.login", "2b6d0e2a29ef926033b07e5ff6218b06b1c805d2")
+//    }
+//}
+
--- src/test/kotlin/com/spring/graphql/GraphqlApplicationTests.kt
+++ src/test/kotlin/com/spring/graphql/GraphqlApplicationTests.kt
@@ -1,13 +1,29 @@
 package com.spring.graphql
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-// @SpringBootTest
+@SpringBootTest
 class GraphqlApplicationTests {
 
-	// @Test
-	fun contextLoads() {
-	}
+    @Test
+    fun graphQLControllerTestCustomersByName() {
+        val graphQLController = GraphQLController()
+
+        val customersByName = graphQLController.customersByName("A")
+
+        Assertions.assertEquals(customersByName[0].name, "A")
+        Assertions.assertEquals(customersByName[0].id, 1)
+    }
+
+    @Test
+    fun graphQLControllerTestCustomers() {
+        val graphQLController = GraphQLController()
+
+        val customersByName = graphQLController.customers()
+
+        Assertions.assertNotNull(customersByName)
+    }
 
 }
